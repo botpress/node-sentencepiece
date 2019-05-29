d366300e620b8e0d42baf9587e967a5565b004b1
Francois <francois_levasseur@hotmail.com>
2019-05-29 15:52:32 -0400

    delete the wrapper in index.js

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
 const sentencepiece = require('./build/Release/sentencepiece.node');
-
-const encode = (inputText, model) => {
-    const modelFilePath = model ? model : `${__dirname}/models/en.model`
-    return sentencepiece.encode(modelFilePath, inputText)
-}
-
-const decode = (pieces, model) => {
-    const modelFilePath = model ? model : `${__dirname}/models/en.model`
-    return sentencepiece.decode(modelFilePath, pieces)
-}
-
-const sentencepieceWrapper = {
-    encode: encode,
-    decode : decode
-}
-
 module.exports = sentencepieceWrapper
