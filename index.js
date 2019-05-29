7d96e1c2bd0b3536e23ebcad71b47378d9ae4980
Francois <francois_levasseur@hotmail.com>
2019-05-29 08:58:07 -0400

    only english model in package which is used as default if no model specified

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const sentencepiece = require('./build/Release/sentencepiece.node');
 
-const mapLanguageToModel = (language) => {
-    let modelFilePath = `${__dirname}/models/`;
-    if (language === 'en') {
-        modelFilePath += 'en.wiki.bpe.vs200000.model'
-    } else if (language === 'fr') {
-        modelFilePath += 'fr.wiki.bpe.vs200000.model'
-    } else  {
-        throw `${language} is not a supported language`
-    }
-    return modelFilePath
-}
-
-const wrappedEncode = (language, inputText) => {
-    const modelFilePath = mapLanguageToModel(language)
+const encode = (inputText, model) => {
+    const modelFilePath = model ? model : `${__dirname}/models/en.model`
     return sentencepiece.encode(modelFilePath, inputText)
 }
 
 const sentencepieceWrapper = {
-    encode: wrappedEncode
+    encode: encode
 }
 
 module.exports = sentencepieceWrapper
