119c2bb8542fe29cc1a62cff4052a3b71402615d
Francois <francois_levasseur@hotmail.com>
2019-05-30 12:49:53 -0400

    binding exposes processor class instead of static funcs

--- cppsrc/main.cpp
+++ cppsrc/main.cpp
@@ -1,10 +1,8 @@
 #include <napi.h>
-#include "encode.h"
-#include "decode.h"
+#include "processor.h"
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  encode::Init(env, exports);
-  decode::Init(env, exports);
+  Processor::Init(env, exports);
   return exports;
 }
 
